9a807a72a3b14a30aba72491ef83689f3799e7cd
Adrian <adrianvalverde45@gmail.com>
2022-09-24 10:50:43 -0400

    commit tree 0b40e688a656a5883ad037339200b5b6e32003d4

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+function saturdayFun(activity = `roller-skate`) {
+    return `This Saturday, I want to ${activity}!`
+}
+
+let mondayWork = function (activity = `go to the office`) {
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(visualFlair = `*`){
+    return function (noun = `special`){
+        return `You are ${visualFlair}${noun}${visualFlair}!`
+    }
+}
